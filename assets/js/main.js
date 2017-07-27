c4064dc11ebbc9755edc808b4546d0cd14fd08fb
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-07-27 13:24:38 -0500

    formatted to OOP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,56 @@
-function Player(name) {
+var rPs = {
 
-	this.name = name;
-	this.wins = 0;
-	this.losses = 0;
-	this.ties = 0;
-	this.lastMove = "";
+    enteredText: "",
+    numPlayers: 0,
+
+    init: function() {
+        rPs.clickHandlers();
+    },
+    clickHandlers: function() {
+
+        $(".ready").on("click", function(e) {
+            var curPlayer = $(e.target).attr("data-player");
+            enteredText = $('#' + curPlayer + ' .name').val().trim();
+            if (enteredText == "") {
+                alert("Please enter a valid name")
+            } else {
+                rPs.numPlayers++;
+                rPs.players[curPlayer] = new rPs.Player(enteredText);
+                $('#' + curPlayer + ' .form-group').slideUp();
+                $('#' + curPlayer + ' .move-text').html("Waiting for 2nd player");
+                rPs.checkPlayers();
+            }
+        });
+
+
+        $(".move").on("click", function(e) {
+            var curPlayer = $(e.target.offsetParent).attr('id');
+            rPs.players[curPlayer].move = $(e.target).attr("data-move")
+            console.log(curPlayer + ":" + rPs.players[curPlayer].move);
+        });
+    },
+    checkPlayers: function() {
+        for (key in rPs.players) {
+            if (rPs.players[key] == null) {
+                $("#" + key + " .moves :button").attr('disabled', true);
+            } else if (rPs.numPlayers > 1) {
+                $("#" + key + " .moves :button").removeAttr('disabled');
+                $('#' + key + ' .move-text').html("Make your move:");
+                $('#' + key + ' .player-title').html("Player: " + rPs.players[key].name);
+            }
+        }
+    }, //checkPlayers function
+    Player: function(name) {
+
+        this.name = name;
+        this.wins = 0;
+        this.losses = 0;
+        this.ties = 0;
+        this.lastMove = "";
+    },
+    players: { player1: null, player2: null }
 }
 
-var enteredText = "",
-    players = { player1: null, player2: null },
-    numPlayers = 0;
 $(document).ready(function() {
-
-    $(".ready").on("click", function(e) {
-        var curPlayer = $(e.target).attr("data-player");
-        enteredText = $('#' + curPlayer + ' .name').val().trim();
-        if (enteredText == "") {
-            alert("Please enter a valid name")
-        } else {
-            numPlayers++;
-            players[curPlayer] = new Player(enteredText);
-            $('#' + curPlayer + ' .form-group').slideUp();
-            $('#' + curPlayer + ' .move-text').html("Waiting for 2nd player");
-            checkPlayers();
-        }
-    });
-
-
-	$(".move").on("click", function(e){
-		var curPlayer = $(e.target.offsetParent).attr('id');
-        players[curPlayer].move = $(e.target).attr("data-move")
-		console.log(curPlayer+":"+players[curPlayer].move);
-	});
-
-});//document.ready
-
-
-function checkPlayers() {
-    for (key in players) {
-        if (players[key] == null) {
-            $("#" + key + " .moves :button").attr('disabled', true);
-        } else if (numPlayers > 1) {
-            $("#" + key + " .moves :button").removeAttr('disabled');
-            $('#' + key + ' .move-text').html("Make your move:");
-            $('#' + key + ' .player-title').html("Player: " + players[key].name);
-        }
-    }
-} //checkPlayers function
+    rPs.init();
+}); //document.ready
