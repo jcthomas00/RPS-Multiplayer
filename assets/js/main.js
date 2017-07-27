b8f1468a83b7357c79a38b83ef89a9a780790b58
Jacob Thomas <jcthomas00@gmail.com>
2017-07-27 12:41:58 -0500

    button class modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,24 @@ function Player(name) {
 	this.wins = 0;
 	this.losses = 0;
 	this.ties = 0;
+	this.lastMove = "";
 }
 var enteredText = "", players = {player1:null, player2:null}, numPlayers = 0;
 $(document).ready(function(){
+
+	$(".ready").on("click", function(e){
+		var curPlayer = $(e.target).attr("data-player");
+		enteredText = $('#'+curPlayer+' .name').val().trim();
+		if(enteredText==""){
+			alert("Please enter a valid name")
+		}else{
+				numPlayers++;
+				players[curPlayer] = new Player(enteredText);
+				$('#'+curPlayer+' .form-group').slideUp();
+				$('#'+curPlayer+' .move-text').html("Waiting for 2nd player");
+				checkPlayers();
+		}
+	});
 
 	$(".ready").on("click", function(e){
 		var curPlayer = $(e.target).attr("data-player");
